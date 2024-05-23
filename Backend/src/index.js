6e4a0cadeadd04ba11e65bd3f6e1ee04e1849a86
Shilpy <79649004+shili-51@users.noreply.github.com>
2024-05-23 19:49:44 +0000

    connect to mongo atlas

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,14 @@
 // IIFE - execute our function immediately
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
 
 
 
