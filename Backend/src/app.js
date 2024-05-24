cb29c5a07480edabdd24aea05b8767beb785aa0d
Shilpy <79649004+shili-51@users.noreply.github.com>
2024-05-24 12:47:49 +0000

    utils code

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,26 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
+
+// different origins from which our frontend accepts request
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+// limit of sending json data to backend
+app.use(express.json({limit: "16kb"}))
+
+// we receive spaces in url etc in order to encode them with % etc to send to backend
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+
+// static - to store files folder containing images, favicon etc
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+
 
 export { app }
