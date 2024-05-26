89ad1d53482e24f2959d3e870aab91ada6291633
Shilpy <79649004+shili-51@users.noreply.github.com>
2024-05-26 12:06:18 +0000

    Router and Controller

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -23,4 +23,21 @@ app.use(cookieParser())
 
 
 
+
+
+// routes import
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
+
+
+
+
+
+
+
+
 export { app }
