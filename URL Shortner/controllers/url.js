0a875d2ae258f49b8c23b3e9413e544cc6ca716b
Shilpy <79649004+shili-51@users.noreply.github.com>
2026-08-19 17:33:28 +0530

    controllers

---
+++ URL Shortner/controllers/url.js
@@ -0,0 +1,42 @@
+const { nanoid } = require("nanoid");
+const URL = require("../models/url");
+
+async function handleGenerateNewShortURL(req, res) {
+    const body = req.body;
+    if (!body.url) return res.status(400).json({ error: "URL is required" });
+    const shortID = nanoid(8);
+    await URL.create({
+        shortId: shortID,
+        redirectURL: body.url,
+        visitedHistory: []
+    })
+    return res.json({ id: shortID });
+}
+
+async function handleGetRedirectedURL(req, res) {
+    const shortId = req.params.shortId;
+    if (!shortId) return res.status(400).json({ error: "Invalid short ID" });
+    const entry = await URL.findOneAndUpdate({
+        shortId
+    }, 
+    {
+        $push: {
+            visitedHistory: {
+                timestamp: Date.now(),
+            }
+        }
+    })
+    res.redirect(entry.redirectURL);
+}
+
+async function handleGetAnalytics(req, res){
+    const shortId = req.params.shortId;
+    const result = await URL.findOne( { shortId } );
+    return res.json({ totalClicks : result.visitedHistory.length, analytics : result.visitedHistory});
+
+}
+module.exports = {
+    handleGenerateNewShortURL,
+    handleGetRedirectedURL,
+    handleGetAnalytics
+}
